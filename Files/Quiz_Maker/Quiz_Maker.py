c9e9e1710719af4d62b64d47ab48876019ccb016
Datt0e <thanhdat.luong248@gmail.com>
2023-11-02 14:38:03 +0100

    Creates new file with quiz.

--- Files/Quiz_Maker/Quiz_Maker.py
+++ Files/Quiz_Maker/Quiz_Maker.py
@@ -1,5 +1,7 @@
 from random import randint
 
+quiz_contents = []
+random_num = []
 num_questions = int(input("Number of questions: "))
 
 with open("Questions.txt", "r") as questions:
@@ -7,5 +9,16 @@ with open("Questions.txt", "r") as questions:
     length = len(contents)
 
     for i in range(num_questions): 
-        #Get random questions of files num_questions times
-        pass
+        while True:    
+            chosen_ques = randint(0,length - 1)
+            if chosen_ques in random_num:
+                continue
+            else:
+                break
+        random_num.append(chosen_ques)
+        quiz_contents.append(contents[chosen_ques])
+
+with open("Quiz.txt","w") as quiz:  
+    for question in quiz_contents:
+        quiz.write(question)
+
